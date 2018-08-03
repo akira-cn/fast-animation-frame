22c4ff953e013bd7dca54c325ef191b5414397f5
akira-cn <akira.cn@gmail.com>
2018-08-03 14:46:56 +0800

    update eslint

--- src/index.js
+++ src/index.js
@@ -1,60 +1,60 @@
 /* eslint-disable no-undef */
 function nowtime() {
   if(typeof performance !== 'undefined' && performance.now) {
-    return performance.now()
-  } else if(typeof process !== 'undefined' && process.hrtime) {
-    const [s, ns] = process.hrtime()
-    return s * 1e3 + ns * 1e-6
+    return performance.now();
+  } if(typeof process !== 'undefined' && process.hrtime) {
+    const [s, ns] = process.hrtime();
+    return s * 1e3 + ns * 1e-6;
   }
-  return Date.now ? Date.now() : (new Date()).getTime()
+  return Date.now ? Date.now() : (new Date()).getTime();
 }
 /* eslint-enable no-undef */
 
 let _requestAnimationFrame,
-  _cancelAnimationFrame
+  _cancelAnimationFrame;
 
 if(typeof global.requestAnimationFrame === 'undefined') {
   _requestAnimationFrame = function (fn) {
     return setTimeout(() => {
-      fn(nowtime())
-    }, 16)
-  }
+      fn(nowtime());
+    }, 16);
+  };
   _cancelAnimationFrame = function (id) {
-    return clearTimeout(id)
-  }
+    return clearTimeout(id);
+  };
 } else {
-  _requestAnimationFrame = global.requestAnimationFrame
-  _cancelAnimationFrame = global.cancelAnimationFrame
+  _requestAnimationFrame = global.requestAnimationFrame;
+  _cancelAnimationFrame = global.cancelAnimationFrame;
 }
 
-const steps = new Map()
-let timerId
+const steps = new Map();
+let timerId;
 
 const requestAnimationFrame = (step) => {
-  const id = Symbol('requestId')
-  steps.set(id, step)
+  const id = Symbol('requestId');
+  steps.set(id, step);
 
   if(timerId == null) {
     timerId = _requestAnimationFrame((t) => {
       timerId = null
       ;[...steps].forEach(([id, callback]) => {
-        callback(t)
-        steps.delete(id)
-      })
-    })
+        callback(t);
+        steps.delete(id);
+      });
+    });
   }
-  return id
-}
+  return id;
+};
 
 const cancelAnimationFrame = (id) => {
-  steps.delete(id)
+  steps.delete(id);
   if(!steps.size && timerId) {
-    _cancelAnimationFrame(timerId)
-    timerId = null
+    _cancelAnimationFrame(timerId);
+    timerId = null;
   }
-}
+};
 
 export {
   requestAnimationFrame,
   cancelAnimationFrame,
-}
+};
